4d6534f9ce4c016453580ca9d41ffa82f107bc52
Merlin Roe <merlin.roe@hotmail.co.uk>
2022-05-21 21:28:38 +0100

    Updated API to use correct way of app cfg in blueprint

--- desktop_service/app/lib/api/cfg_api.py
+++ desktop_service/app/lib/api/cfg_api.py
@@ -1,13 +1,12 @@
 from app.lib.plugin_loader import plugin_list
-from config.config import load_secret_key
-from flask import Blueprint, jsonify
+from flask import Blueprint, current_app, jsonify
 
 cfg_api = Blueprint("cfg", __name__, url_prefix="/api")
 
 
 @cfg_api.route("/key", methods=["GET"])
 def get_key():
-    return (jsonify(load_secret_key()), 200)
+    return (jsonify(current_app.secret_key), 200)
 
 
 @cfg_api.route("/plugins", methods=["GET"])

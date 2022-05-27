eff439db4824f3e454a5413dc89ea76a6d4981ae
Merlin Roe <merlin.roe@hotmail.co.uk>
2022-05-27 14:28:18 +0100

    Redid pluginManager to be lessspaghetti

--- app/lib/plugin_control.py
+++ app/lib/plugin_control.py
@@ -2,7 +2,7 @@ import importlib
 import os
 import sqlite3
 from pathlib import Path
-from typing import Dict
+from typing import Callable, Dict, Union
 
 import git
 import toml
@@ -18,35 +18,19 @@ class pluginManager:
         self.load_plugins_from_DB()
         self.import_plugins()
         self.load_plugins()
+        print(self.plugins)
 
-    def load_plugins_from_DB(self):
-        try:
-            connection = get_config_db()
-            connection.row_factory = sqlite3.Row
-            cur = connection.cursor()
-
-            sql = f"SELECT * FROM plugins"
-            cur.execute(sql)
-            existing = cur.fetchall()
-            for plugin in existing:
-                self.plugins[plugin["name"]] = dict(zip(plugin.keys(), plugin))
-
-            for name, data in self.plugins.copy().items():
-                if not os.path.exists(data["path"]):
-                    print(f"Module {name} Missing... Deleting SQL record")
-                    sql = f"DELETE FROM plugins WHERE id = {data['id']};"
-                    cur.execute(sql)
-                    connection.commit()
-                    self.plugins.pop(name)
-
-        except Exception as e:
-            print("Failed to load plugin DB")
-            print(e)
-        finally:
-            connection.close()
+    def find_plugins(self) -> Dict[str, str]:
+        plugins = {}
+        for dir in os.listdir(self.plugins_dir):
+            full_path = os.path.join(self.plugins_dir, dir)
+            if os.path.isdir(full_path):
+                for file in Path(full_path).rglob("registry.toml"):
+                    plugins[dir] = os.path.dirname(file)
+        return plugins
 
     def import_plugins(self):
-        for name, path in self.plugin_list().items():
+        for name, path in self.find_plugins().items():
             registry = self.get_plugin_registry(name, path)
             if not registry:
                 print(f"Unknown {name} Plugin has no registry File. Skipping")
@@ -54,27 +38,31 @@ class pluginManager:
             elif registry["name"] not in self.plugins.keys():
                 self.save_plugin_register(registry)
 
-            plugin_import = self.load_plugin_module(registry["name"], path)
-            if not plugin_import:
-                print(f"Skipping {name} [{registry['name']}] Due to import failure")
+            plugin_setup, plugin_blueprint = self.import_plugin_module(name, path)
+
+            if not plugin_blueprint:
+                print(f"Skipping {name} [{registry['name']}] Due to no blueprint found")
                 continue
 
             self.plugins[name] = {
-                **self.plugins[name],
-                "path": path,
-                "setup": plugin_import[0],
-                "blueprint": plugin_import[1],
+                **registry,
+                "setup": plugin_setup,
+                "blueprint": plugin_blueprint,
             }
 
-    def load_plugin_module(self, module_name: str, path: str) -> Blueprint:
+    def import_plugin_module(
+        self, module_name: str, path: str
+    ) -> Union[Callable, Blueprint]:
         path = path.replace("/", ".")
+        module_file = path.split(".")[-1]
         try:
-            plugin_api = importlib.import_module(f".{module_name}", path).api
+            plugin_api = importlib.import_module(f".{module_file}", path).api
             try:
-                plugin_setup = importlib.import_module(f".{module_name}", path).setup
+                plugin_setup = importlib.import_module(f".{module_file}", path).setup
             except AttributeError:
                 print(f"{module_name} has no setup function")
                 plugin_setup = None
+            plugin_api.name = module_name
             plugin_api.static_folder = "./src/static"
             plugin_api.template_folder = "./src"
             plugin_api.url_prefix = f"/{module_name}"
@@ -104,43 +92,6 @@ class pluginManager:
 
             self.plugins[name]["pages"] = pages
 
-    def save_plugin_register(self, registry: dict):
-        try:
-            connection = get_config_db()
-            cur = connection.cursor()
-            sql = f"INSERT INTO plugins (name, path, title, description, author, github, autoUpdate, tag, version) "
-            sql += f"VALUES('{registry['name']}', '{registry['path']}', '{registry['title']}', '{registry['description']}', '{registry['author']}', '{registry['github']}', 1, '{registry['tag']}','{registry['version']}');"
-            cur.execute(sql)
-            connection.commit()
-        except Exception as e:
-            print("Failed to save plugin registry")
-            print(e)
-            connection.rollback()
-        finally:
-            self.plugins[registry["name"]] = registry
-            connection.close()
-
-    def run_plugins_setup(self):
-        for plugin in self.plugins.values():
-            if plugin.get("setup"):
-                plugin["setup"](self.app)
-
-    def plugin_list(self) -> dict:
-        plugins = {}
-        for dir in os.listdir(self.plugins_dir):
-            if os.path.isdir(os.path.join(self.plugins_dir, dir)) and dir[0] != "_":
-                for file in Path(os.path.join(self.plugins_dir, dir)).rglob(
-                    "registry.toml"
-                ):
-                    plugins[os.path.basename(os.path.dirname(file))] = os.path.dirname(
-                        file
-                    )
-        return plugins
-
-    def find_registry(self, repo: str) -> str:
-        for file in Path(repo).rglob("registry.toml"):
-            return file.as_posix()
-
     def get_plugin_registry(self, name: str, path: str) -> dict:
         registry = os.path.join(path, "registry.toml")
         if os.path.exists(registry):
@@ -150,9 +101,92 @@ class pluginManager:
             return config["plugin"]
         return None
 
+    def run_plugins_setup(self):
+        for plugin in self.plugins.values():
+            if plugin.get("setup"):
+                plugin["setup"](self.app)
+
+    def load_plugins_from_DB(self):
+        try:
+            connection = get_config_db()
+            connection.row_factory = sqlite3.Row
+            cur = connection.cursor()
+
+            cur.execute("SELECT * FROM plugins")
+            existing = cur.fetchall()
+
+            for plugin in existing:
+                self.plugins[plugin["name"]] = dict(zip(plugin.keys(), plugin))
+
+        except Exception as e:
+            print("Failed to load plugin DB")
+            print(e)
+        finally:
+            connection.close()
+
+        self.remove_missing_plugins()
+
+    def remove_missing_plugins(self):
+        try:
+            connection = get_config_db()
+            connection.row_factory = sqlite3.Row
+            cur = connection.cursor()
+            cur.execute("SELECT * FROM plugins")
+
+            for row in cur.fetchall():
+                if not os.path.exists(row["path"]):
+                    print(f"Module {row['name']} Missing... Deleting SQL record")
+                    sql = f"DELETE FROM plugins WHERE id = {row['id']};"
+                    cur.execute(sql)
+                    connection.commit()
+                    self.plugins.pop(row["name"])
+
+        except Exception as e:
+            print("Failed to load plugin DB")
+            print(e)
+        finally:
+            connection.close()
+
+    def save_plugin_register(self, registry: dict):
+        try:
+            connection = get_config_db()
+            cur = connection.cursor()
+            sql = f"INSERT INTO plugins (name, path, title, description, author, github, autoUpdate, tag, version) VALUES (?,?,?,?,?,?,?,?,?);"
+            cur.execute(
+                sql,
+                (
+                    registry["name"],
+                    registry["path"],
+                    registry["title"],
+                    registry["description"],
+                    registry["author"],
+                    registry["github"],
+                    self.app.config["config"]["autoUpdatePlugins"],
+                    registry["tag"],
+                    registry["version"],
+                ),
+            )
+            connection.commit()
+        except Exception as e:
+            print("Failed to save plugin registry")
+            print(e)
+            connection.rollback()
+        finally:
+            self.plugins[registry["name"]] = registry
+            connection.close()
+
+    def find_registry(self, repo: str) -> str:
+        for file in Path(repo).rglob("registry.toml"):
+            return file.as_posix()
+
     def plugin_install(self, link: str):
-        tmp_repo_path = os.path.join(self.plugins_dir, "new_repo")
-        git.Repo.clone_from(link, tmp_repo_path)
+        tmp_repo_path = os.path.join(self.plugins_dir, "new_plugin")
+        try:
+            git.Repo.clone_from(link, tmp_repo_path)
+        except Exception as e:
+            print(e)
+            print(f"Failed to clone repo: {link}")
+            return
 
         accountName = link.split("/")[-2]
         registry_path = self.find_registry(tmp_repo_path)

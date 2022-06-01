0475a01213920535000a817203cdd2ecc9db191c
Merlin Roe <merlin.roe@hotmail.co.uk>
2022-06-01 19:28:21 +0100

    Added Fuzzy index so url's can contain url arguments

--- open_desk_dash/service.py
+++ open_desk_dash/service.py
@@ -4,6 +4,7 @@ from werkzeug.urls import url_parse
 from open_desk_dash.config import cfg_api
 from open_desk_dash.lib.db_control import setup_DB_control
 from open_desk_dash.lib.plugin_control import PluginManager
+from open_desk_dash.lib.update_control import update_check
 
 ODDash = Flask(
     "OpenDeskDash",
@@ -35,27 +36,28 @@ def page_not_found(e):
     return redirect(ODDash.config["config"]["pages"][0])
 
 
+def fuzzy_index(srch: str, within: list) -> int:
+    for i, page in enumerate(within):
+        if srch in page:
+            return i
+    return 0
+
+
 @ODDash.route("/next", methods=["GET"])
 def next_dash():
     prev_page = url_parse(request.referrer)
-    try:
-        indx = ODDash.config["config"]["pages"].index(prev_page.path)
-        indx += 1
-        if indx == len(ODDash.config["config"]["pages"]):
-            indx = 0
-    except IndexError:
+    indx = fuzzy_index(prev_page.path, ODDash.config["config"]["pages"])
+    indx += 1
+    if indx == len(ODDash.config["config"]["pages"]):
         indx = 0
     return redirect(ODDash.config["config"]["pages"][indx])
 
 
 @ODDash.route("/prev", methods=["GET"])
 def prev_dash():
-    try:
-        prev_page = url_parse(request.referrer)
-        indx = ODDash.config["config"]["pages"].index(prev_page.path)
-        indx -= 1
-        if indx < 0:
-            indx = len(ODDash.config["config"]["pages"]) - 1
-    except IndexError:
-        indx = 0
+    prev_page = url_parse(request.referrer)
+    indx = fuzzy_index(prev_page.path, ODDash.config["config"]["pages"])
+    indx -= 1
+    if indx < 0:
+        indx = len(ODDash.config["config"]["pages"]) - 1
     return redirect(ODDash.config["config"]["pages"][indx])
